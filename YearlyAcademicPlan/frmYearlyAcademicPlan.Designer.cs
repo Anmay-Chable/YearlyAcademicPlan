2544d3c07298c7e511c6fa071563850af008aad4
Angel May <Anmaychable@gmail.com>
2025-04-16 09:55:15 -0700

    Add project files.

---
+++ YearlyAcademicPlan/frmYearlyAcademicPlan.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace YearlyAcademicPlan
+{
+    partial class frmYearlyAcademicPlan
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            btnAdd = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            btnExit = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(88, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(88, 66);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 39);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(88, 122);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(200, 39);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(88, 179);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(200, 39);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(88, 236);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(200, 39);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(88, 291);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(200, 39);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(88, 345);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(200, 39);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(88, 404);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(200, 39);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(88, 459);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(200, 39);
+            textBox9.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(388, 12);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(150, 46);
+            btnAdd.TabIndex = 9;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(388, 87);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(150, 46);
+            btnDelete.TabIndex = 10;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(388, 161);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(150, 46);
+            btnClear.TabIndex = 11;
+            btnClear.Text = "Clear All";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(388, 236);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(150, 46);
+            btnExit.TabIndex = 12;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += this.btnExit_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(4, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 32);
+            label1.TabIndex = 13;
+            label1.Text = "1.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(4, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 32);
+            label2.TabIndex = 14;
+            label2.Text = "2.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(4, 129);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 32);
+            label3.TabIndex = 15;
+            label3.Text = "3.";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(4, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 32);
+            label4.TabIndex = 16;
+            label4.Text = "4.";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(4, 239);
+            label5.Name = "label5";
+            label5.Size = new Size(32, 32);
+            label5.TabIndex = 17;
+            label5.Text = "5.";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(4, 298);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 32);
+            label6.TabIndex = 18;
+            label6.Text = "6.";
+            label6.Click += this.label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(4, 352);
+            label7.Name = "label7";
+            label7.Size = new Size(32, 32);
+            label7.TabIndex = 19;
+            label7.Text = "7.";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(4, 411);
+            label8.Name = "label8";
+            label8.Size = new Size(32, 32);
+            label8.TabIndex = 20;
+            label8.Text = "8.";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(4, 466);
+            label9.Name = "label9";
+            label9.Size = new Size(32, 32);
+            label9.TabIndex = 21;
+            label9.Text = "9.";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(4, 550);
+            label10.Name = "label10";
+            label10.Size = new Size(151, 32);
+            label10.TabIndex = 22;
+            label10.Text = "Total Credits:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(4, 607);
+            label11.Name = "label11";
+            label11.Size = new Size(283, 32);
+            label11.TabIndex = 23;
+            label11.Text = "Total Credits Completed: ";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(275, 550);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(114, 39);
+            textBox10.TabIndex = 24;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(275, 604);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(114, 39);
+            textBox11.TabIndex = 25;
+            // 
+            // frmYearlyAcademicPlan
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(568, 683);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnExit);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "frmYearlyAcademicPlan";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button btnExit;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox10;
+        private TextBox textBox11;
+    }
+}
---
+++ YearlyAcademicPlan/frmYearlyAcademicPlan.cs
@@ -0,0 +1,10 @@
+namespace YearlyAcademicPlan
+{
+    public partial class frmYearlyAcademicPlan : Form
+    {
+        public frmYearlyAcademicPlan()
+        {
+            InitializeComponent();
+        }
+    }
+}
