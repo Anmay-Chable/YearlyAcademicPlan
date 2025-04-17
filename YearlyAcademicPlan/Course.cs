a18423ec57f85cb0f764b7a29a4fe20b5b76a0f1
Angel May <Anmaychable@gmail.com>
2025-04-17 11:06:29 -0700

    Created Course struct and properties and construct and enum.

---
+++ YearlyAcademicPlan/Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearlyAcademicPlan
+{
+    public struct Course
+    {
+        /// Properties
+        public string CourseName { get; set; }
+        public int CourseCredits { get; set; }
+        public Status CourseStatus { get; set; }
+        public string PrecedingCourseName { get; set; }
+        public string FollowingCourseName { get; set; }
+
+            /// Constructor
+        public Course (string name, int credits, Status status, string precedingCourse = null, string followingCourse = null)
+        {
+            CourseName = name;
+            CourseCredits = credits;
+            CourseStatus = status;
+            PrecedingCourseName = precedingCourse;
+            FollowingCourseName = followingCourse;
+        }
+    }
+
+    /// Enum for course status
+    public enum Status
+    {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+}
